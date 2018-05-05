332780fe7d4b6a37e3e3f0fdf17707b09e09946f
jendatx <jend@berkeley.edu>
2018-05-05 15:58:25 -0500

    begin sample population

--- make_person_need.py
+++ make_person_need.py
@@ -1,14 +1,34 @@
 from datetime import datetime
+import sqlite3
+
+# Sample data
+person = 'Maria Garcia'
+location = 'Onion Creek'
+status = 'OK'
+ppl_count = 4
+is_vulnerable = 0
 
 start_time = datetime.now()
 start_timestamp = str(start_time)
 
-# create person & fetch person # id
+# connect to database
+conn = sqlite3.connect('disaster_db.sqlite')
+cur = conn.cursor()
+
+# create party & fetch party # id
+cur.execute('''INSERT INTO Parties (name) \
+            VALUES ( ?, )''',\
+            (person,) )
+cur.execute('SELECT id FROM Parties \
+            WHERE name = ? ', (person, ))
+gotid = cur.fetchone()
+print(gotid)
+
 # create status: OK or HELP
 
 # create need with timestamp
-cur.execute('''INSERT INTO Needs (colname) \
-            VALUES ( ?, ?, ?, ? )''',\
-            ( start_timestamp) )
-cur.execute('SELECT need_id FROM Needs \
-            WHERE Player_start_timestamp = ? ', (start_timestamp, ))
+# cur.execute('''INSERT INTO Needs (colname) \
+#             VALUES ( ?, ?, ?, ? )''',\
+#             ( start_timestamp) )
+# cur.execute('SELECT need_id FROM Needs \
+#             WHERE Player_start_timestamp = ? ', (start_timestamp, ))
