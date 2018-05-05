e3dbe12b1aa6ab66173149daf5848ba8796f8ea6
Allistair Pinsof <labcoatdisaster@gmail.com>
2018-05-05 13:08:28 -0500

    Fixed folder structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-
-import './App.css';
+// import Map from '../Map';
+import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-               
+        TEST!
+        {/* <Map /> */}
       </div>
     );
   }
