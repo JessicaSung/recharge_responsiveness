763f997854ab4ec3a00ebbb20b86e3b25af8196c
JessicaSung <18690072+JessicaSung@users.noreply.github.com>
2018-05-05 15:43:18 -0500

    reverted some file structure change

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import Map from './Map';
-import '../css/App.css';
+import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* // test */}
-        <Map />
+
       </div>
     );
   }
