d0b29eacf4b59cecc8f346cc72965cce145ad078
Ken <ken@mainsequencesoftware.com>
2018-05-05 11:02:19 -0500

    react files

---
+++ maps/src/App.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+                <form>
+                  <div className="formDiv"> 
+          <label>Location</label>
+          <input type='text' name='location'/>
+          </div>
+          <div className='formDiv'>
+          <label>Number of people at location</label>
+          <input type='text'name='number'/>
+          </div>
+          <div className='formDiv'>
+          <label>Do you have elderly/diabled/infants in your party?</label>
+          <input type='radio' name='yesDisabled' value='Yes'/>
+          <label>Yes</label>
+          <input type='radio' name='noDisabled' value='No'/>
+          <label>No</label>
+          </div>
+          <div className='formDiv'>
+          <label>If you answered yes to the above question please give a brief description of the party members</label>
+          <textarea className='formDiv'></textarea>
+          </div>
+          <div className='formDiv'>
+          <label>Are there any injured parties?</label>
+          <input type='radio' name='yesInjured' value='Yes'/>
+          <label>Yes</label>
+          <input type='radio' name='noInjured' value='No'/>
+          <label>No</label>
+          </div>
+          <div className='formDiv'>
+          <label>If there are injured parties please give a brief description of the injuries</label>
+          <textarea className='formDiv'></textarea>
+          </div>
+          <div className='formDiv'>
+          <label>Does anyone need medication?</label>
+          <input type='radio' name='yesMeds' value='Yes'/>
+          <label>Yes</label>
+          <input type='radio' name='noMeds' value='No'/>
+          <label>No</label>
+          </div>
+          <div className='formDiv'>
+          <label>If anyone needs medication please give a brief description of the medication needed</label>
+          <textarea className='formDiv'></textarea>
+          </div>
+<div className='formDiv'>
+          <label>Do you have power?</label>
+          <input type='radio' name='yesPower' value='Yes'/>
+          <label>Yes</label>
+          <input type='radio' name='noPower' value='No'/>
+          <label>No</label>
+          </div>
+          <div className='formDiv'>
+          <label>Do you have heat/air conditioning?</label>
+          <input type='radio' name='yesHeat' value='Yes'/>
+          <label>Yes</label>
+          <input type='radio' name='noHeat' value='No'/>
+          <label>No</label>
+          </div>
+          <div className='formDiv'>
+          <label>Do you have food and water?</label>
+          <input type='radio' name='yesFood' value='Yes'/>
+          <label>Yes</label>
+          <input type='radio' name='noFood' value='No'/>
+          <label>No</label>
+          <label>If you answered no to the above question please give a brief description of your needs</label>
+          <textarea className='formDiv'></textarea>
+          </div>
+          <div className='formDiv'>
+          <label>Do you have any pets with you?</label>
+          <input type='radio' name='yesPets' value='Yes'/>
+          <label>Yes</label>
+          <input type='radio' name='noPets' value='No'/>
+          <label>No</label>
+          </div>
+          <div className='formDiv'>
+          <label>If you answered no to the above question please give a brief description of your pets</label>
+          <textarea className='formDiv'></textarea>
+          </div>
+
+          
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
