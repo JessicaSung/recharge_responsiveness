b768e8b7ae50e903c3b061757405b5e84bff3d0e
JessicaSung <18690072+JessicaSung@users.noreply.github.com>
2018-05-06 10:42:05 -0500

    added responders page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from './components/form'
+import Responders from './components/Responders'
 
 class App extends Component {
   render() {
     return (
       <div>
-<Form />
+        <Form />
+        <Responders />
       </div>
     );
   }
