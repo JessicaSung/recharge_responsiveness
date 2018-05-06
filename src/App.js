7d4cd4627a09222c6132745ad72c91013e10b1be
JessicaSung <18690072+JessicaSung@users.noreply.github.com>
2018-05-06 12:42:58 -0500

    moved body content above footer and made map larger

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ import NavBar from './components/NavBar'
 class App extends Component {
   constructor(){
     super()
-   this.state={
-     userInfo:[]
-   }
+    this.state={
+      userInfo:[]
+    }
   }
   // componentDidMount(){
   //   axios.get('http://localhost:5000/userInfo')
@@ -32,54 +32,52 @@ class App extends Component {
   
   submitForm=(e)=>{
     console.log('function accessed')
-e.preventDefault()
-let name=e.target.name.value
-let address=e.target.address.value
-let meds=e.target.Meds.value
-let medComment=e.target.medComment.value
-let power=e.target.Power.value
-let powComment=e.target.powerComment.value
-let pets=e.target.Pets.value
-let petComment=e.target.petComment.value
-let food=e.target.Food.value
-let foodComment=e.target.foodComment.value
-let injured=e.target.Injured.value
-let injComment=e.target.injComment.value
-let number=e.target.number.value
-let status=e.target.status.value
-let heat=e.target.Heat.value
-let heatComment=e.target.heatComment.value
-let disabled=e.target.Disabled.value
-let disComment=e.target.disComment.value
-let other=e.target.other.value
-let userInfo={name:name, address:address, meds:meds, medsComment:medComment,power:power, powerComment:powComment,pets:pets, petComment:petComment, food:food, foodComment:foodComment,
-injured:injured, injComment:injComment, number:number, status:status, heat:heat, heatComment:heatComment, disabled:disabled, disComment:disComment, other:other}
-//axios.post('http://localhost:5000/userInfo', userInfo)
-console.log(userInfo)
+    e.preventDefault()
+    let name=e.target.name.value
+    let address=e.target.address.value
+    let meds=e.target.Meds.value
+    let medComment=e.target.medComment.value
+    let power=e.target.Power.value
+    let powComment=e.target.powerComment.value
+    let pets=e.target.Pets.value
+    let petComment=e.target.petComment.value
+    let food=e.target.Food.value
+    let foodComment=e.target.foodComment.value
+    let injured=e.target.Injured.value
+    let injComment=e.target.injComment.value
+    let number=e.target.number.value
+    let status=e.target.status.value
+    let heat=e.target.Heat.value
+    let heatComment=e.target.heatComment.value
+    let disabled=e.target.Disabled.value
+    let disComment=e.target.disComment.value
+    let other=e.target.other.value
+    let userInfo={name:name, address:address, meds:meds, medsComment:medComment,power:power, powerComment:powComment,pets:pets, petComment:petComment, food:food, foodComment:foodComment,
+    injured:injured, injComment:injComment, number:number, status:status, heat:heat, heatComment:heatComment, disabled:disabled, disComment:disComment, other:other}
+    //axios.post('http://localhost:5000/userInfo', userInfo)
+    console.log(userInfo)
   }
+
   render() {
     return (
       <div>
-      {/*Al's map*/}
-        <Header />
-        <NavBar />
-        {/*child component*/}
-        <MapContainer google={this.props.google} />
-        <Footer />
-      {/*end Al's map*/}
-  
         
-<Switch>
-  <Route path='/'exact render={(props)=>(
-    <Home {...props}/>
-  )}/>
-  <Route path='/form'render={(props)=>(
-    <Form {...props}submitForm={this.submitForm}/>
-  )} />
-<Route path='/responders' render={(props)=>(
-  <Responders {...props} userInfo={this.state.usrInfo}/>
-)}/> 
-</Switch>
+          <Header />
+          <NavBar />
+          <MapContainer google={this.props.google} />
+          {/*child component*/}
+            <Switch>
+              <Route path='/' exact render={(props) => (
+                <Home {...props} />
+              )} />
+              <Route path='/form' render={(props) => (
+                <Form {...props} submitForm={this.submitForm} />
+              )} />
+              <Route path='/responders' render={(props) => (
+                <Responders {...props} userInfo={this.state.usrInfo} />
+              )} />
+            </Switch>
+          <Footer /> 
       </div>
     );
   }
