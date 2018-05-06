a0e05560963ee3e35e3d69d17a1b3061ded1d75a
fancyaction <labcoatdisaster@gmail.com>
2018-05-06 06:47:43 -0500

    Add jumbotron

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import { Jumbotron, Grid, Row, Col } from "react-bootstrap";
+import './Header.css';
+
 
 class Header extends Component {
     render () {
         return (
-            <div>
-                <h1> First Response Person Finder </h1>
-            </div>
+            <Grid>
+                <Jumbotron>
+                    <h1 className='headerH1'> First Response Helper</h1>
+                    <p>This is a website for creating a network of disaster survivors for first responders, so in an disaster relief efforts responders can know the status, needs and location of survivors. Responders can also connect survivors with good samaritans who have listed free aid (water, food, clothes) in a survivor's area.</p>
+                </Jumbotron>
+            </Grid>
         )
     }
 }
