7ed915bde4f7b4ad456c95b7ffa12a71982acece
fancyaction <labcoatdisaster@gmail.com>
2018-05-05 19:09:46 -0500

    Add MapContainer to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from './components/form'
+import MapContainer from './components/MapContainer';
 
 class App extends Component {
   render() {
     return (
       <div>
-<Form />
+        <Form />
+        <MapContainer />
       </div>
     );
   }
